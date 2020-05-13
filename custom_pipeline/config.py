a2817cdff56ee1ecdd4190a6bee75d687fba6f91
ezekielolugbami <ezekielolugbami@gmail.com>
2020-05-13 02:51:25 -0700

    update config

--- custom_pipeline/config.py
+++ custom_pipeline/config.py
@@ -8,8 +8,10 @@ numerical_to_imput = ['Age']
 # variables to transform
 numerical_log = ['Age', 'Fare']
 
+
 # variables to encode
 categorical_encode = ['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked']
+
 
 # slected features for training
 features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Ticket', 'Cabin', 'Embarked', 'Age_na']
