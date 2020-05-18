ffbd73e81858f0b3833414abf28c9af707420223
ezekielolugbami <ezekiel.olugbami@gmail.com>
2020-05-18 07:30:36 +0100

    update config

--- third_party_pipeline/config.py
+++ third_party_pipeline/config.py
@@ -8,11 +8,18 @@ target = 'Survived'
 # input variables
 features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Ticket', 'Cabin', 'Embarked', 'Age_na']
 
+# drop unnecessary features
+drop_features = ''
+
 # numerical variables with na in train set
 numerical_vars_with_na = ['Age']
 
 # categorical variables with na in train set
 categorical_vars_with_na = ['Cabin', 'Embarked']
+
+# temporal variable calculation
+# temporal_vars = 'Age'
+
 
 # variables to log transform
 numerical_log_vars = ['Age', 'Fare']
